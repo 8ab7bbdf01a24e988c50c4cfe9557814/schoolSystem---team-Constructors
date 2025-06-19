ea1b44bf39f8443ada4db5a1f8b97ef8c3493032
K8 <kate.cochon@education.nsw.gov.au>
2025-06-19 14:53:10 +1000

    Update student.py

--- student.py
+++ student.py
@@ -2,5 +2,24 @@
 #Accept and assign agreed parameters in the constructor and define agreed public methods in the class
 #Program the class so that users of the class only need to interact with its methods
 
-kate was here:)
+#kate was here:)
 
+import Person
+
+class Student(Person):
+    test = 1
+    def __init__(self, name, schoolYear):
+        super().__init__(name)
+        self.schoolYear=schoolYear
+        
+    
+    def getSchoolYear(self, schoolYear):
+        print(f"{self.Person} school year is {current_Year}")
+     
+    def setSchoolYear(self, schoolYear):
+        current_year=input("Set school year: ")
+        
+    def nextSchoolYear(self, schoolYear):
+        next_year=current_year+1
+        print(f"{self.Person} is gonna be in year {next_year} next year")
+      
